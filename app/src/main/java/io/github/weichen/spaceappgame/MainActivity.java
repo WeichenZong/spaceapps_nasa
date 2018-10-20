93f88e264b618f5fd4a4899d2fd6ba1cd0486467
darkghost <dmytro@perkov.ca>
2018-10-20 12:01:44 -0400

    2 buttons work now

--- app/src/main/java/io/github/weichen/spaceappgame/MainActivity.java
+++ app/src/main/java/io/github/weichen/spaceappgame/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity implements OnClickListener {
 
 
     Button b1;
+    Button b2;
     ImageView iw;
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +22,8 @@ public class MainActivity extends Activity implements OnClickListener {
         b1 = (Button) findViewById(R.id.button1);
         b1.setOnClickListener(this);
 
+        b2 = (Button) findViewById(R.id.button2);
+        b2.setOnClickListener(this);
         iw = (ImageView) findViewById(R.id.icon);
     }
 
@@ -30,6 +33,10 @@ public class MainActivity extends Activity implements OnClickListener {
         if (v == b1)
         {
             iw.setImageResource(R.drawable.realtest0);
+        }
+        else if (v == b2)
+        {
+            iw.setImageResource(R.drawable.icon);
         }
 
     }
