72189f5e5369c8ac54e361aacbeab33b211bdda2
Zeph0528 <zeph0528@yeah.net>
2018-10-20 15:11:55 -0400

    iterations

--- app/src/main/java/io/github/weichen/spaceappgame/MainActivity.java
+++ app/src/main/java/io/github/weichen/spaceappgame/MainActivity.java
@@ -20,6 +20,9 @@ import java.io.InputStream;
 public class MainActivity extends Activity implements OnClickListener {
 
     Map<Integer,String> map = new Hashtable<Integer,String>();
+    Set<Integer> keys;
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    int position = 0;
     Button b1;
     Button b2;
     TextView text;
@@ -43,7 +46,10 @@ public class MainActivity extends Activity implements OnClickListener {
         map.put(R.drawable.galaxy_cluster1, "A galaxy cluster is a structure that consists of hundreds of thousands of galaxies that are bonded together by gravity with typical masses ranging from 10^14 to 10^15 solar masses. They are attracted to each other and form giant clusters that stretch for thousands of light-years. Usually, the galaxies and intergalactic gas take up to 10% of the clusters. The rest is occupied by dark matter and/or nothingness.");
         map.put(R.drawable.spiral_galaxy1, "Spiral galaxies resemble spiraling pinwheels. Though the stars and other visible material contained in such galaxies lie mostly on a plane, the majority of mass in spiral galaxies exists in a roughly spherical halo of dark matter that extends beyond the visible component, as demonstrated by the universal rotation curve concept");
         map.put(R.drawable.spiral_galaxy2, "Spiral galaxies consist of a rotating disk of stars and interstellar medium, along with a central bulge of generally older stars. Extending outward from the bulge are relatively bright arms. In the Hubble classification scheme, spiral galaxies are listed as type S, followed by a letter (a, b, or c) that indicates the degree of tightness of the spiral arms and the size of the central bulge.");
-        Set<Integer> keys = map.keySet();
+        keys = map.keySet();
+        for (int key : keys){
+            list.add(key);
+        }
 
     }
 
@@ -52,13 +58,15 @@ public class MainActivity extends Activity implements OnClickListener {
         // TODO Auto-generated method stub
         if (v == b1)
         {
-            iw.setImageResource(R.drawable.mars);
-            text.setText(map.get(R.drawable.mars));
+            position--;
+            iw.setImageResource(list.get(position));
+            text.setText(map.get(list.get(position)));
         }
         else if (v == b2)
         {
-            iw.setImageResource(R.drawable.icon);
-            text.setText(map.get(R.drawable.icon));
+            position++;
+            iw.setImageResource(list.get(position));
+            text.setText(map.get(list.get(position)));
 
         }
 
