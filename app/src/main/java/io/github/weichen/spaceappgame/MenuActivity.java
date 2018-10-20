e0591f56c3ff0a7da61e37e5d279dcf14807cd62
darkghost <dmytro@perkov.ca>
2018-10-20 19:34:54 -0400

    2 activities

---
+++ app/src/main/java/io/github/weichen/spaceappgame/MenuActivity.java
@@ -0,0 +1,34 @@
+package io.github.weichen.spaceappgame;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends Activity implements View.OnClickListener {
+
+    Button learn;
+    Button test;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        learn = (Button) findViewById(R.id.learn_button);
+        learn.setOnClickListener(this);
+
+        test = (Button) findViewById(R.id.test_button);
+        test.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == learn){
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra( "name", "value");
+            startActivity(intent);
+        }
+    }
+}
