a3873874b64aa3c4e4bdc578aae06e423cdeff86
darkghost <dmytro@perkov.ca>
2018-10-20 13:53:23 -0400

    fixes 2 buttons

--- app/src/main/java/io/github/weichen/spaceappgame/MainActivity.java
+++ app/src/main/java/io/github/weichen/spaceappgame/MainActivity.java
@@ -2,11 +2,18 @@ package io.github.weichen.spaceappgame;
 
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.InputStream;
 
 public class MainActivity extends Activity implements OnClickListener {
 
@@ -18,13 +25,13 @@ public class MainActivity extends Activity implements OnClickListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
-
         b1 = (Button) findViewById(R.id.button1);
         b1.setOnClickListener(this);
 
         b2 = (Button) findViewById(R.id.button2);
         b2.setOnClickListener(this);
         iw = (ImageView) findViewById(R.id.icon);
+
     }
 
     @Override
@@ -32,7 +39,7 @@ public class MainActivity extends Activity implements OnClickListener {
         // TODO Auto-generated method stub
         if (v == b1)
         {
-            iw.setImageResource(R.drawable.realtest0);
+            iw.setImageResource(R.drawable.mars);
         }
         else if (v == b2)
         {
