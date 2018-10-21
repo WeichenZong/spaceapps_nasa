ace9b3bd4efd4390a9b2dd4ffcd49d8cc47a612b
darkghost <dmytro@perkov.ca>
2018-10-20 20:49:52 -0400

    buttons setup

--- app/src/main/java/io/github/weichen/spaceappgame/TestingActivity.java
+++ app/src/main/java/io/github/weichen/spaceappgame/TestingActivity.java
@@ -4,13 +4,26 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class TestingActivity extends Activity implements View.OnClickListener {
 
+    Button a;
+    Button b;
+    Button c;
+    Button d;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testing);
+        a = (Button) findViewById(R.id.button_a);
+        a.setOnClickListener(this);
+        b = (Button) findViewById(R.id.button_b);
+        b.setOnClickListener(this);
+        c = (Button) findViewById(R.id.button_c);
+        c.setOnClickListener(this);
+        d = (Button) findViewById(R.id.button_d);
+        d.setOnClickListener(this);
     }
 
     @Override
