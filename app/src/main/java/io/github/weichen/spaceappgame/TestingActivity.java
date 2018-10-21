f9894fca6b8078e87802835d125f70aa00a84211
Zeph0528 <zeph0528@yeah.net>
2018-10-20 22:51:22 -0400

    read txt file data

--- app/src/main/java/io/github/weichen/spaceappgame/TestingActivity.java
+++ app/src/main/java/io/github/weichen/spaceappgame/TestingActivity.java
@@ -8,6 +8,12 @@ import android.view.View;
 import android.widget.Button;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
@@ -16,20 +22,35 @@ import java.util.Random;
 import java.util.Scanner;
 
 class Triple{
-    String a,b;
+    String a,b1,b2,b3,b4;
     int c;
-    public Triple(String a, String b, int c) {
+    public Triple(String a, String b1,String b2,String b3,String b4 ,int c) {
         this.a= a;
-        this.b = b;
+        this.b1= b1;
+        this.b2=b2;
+        this.b3=b3;
+        this.b4=b4;
         this.c= c;
     }
     public String getQustions(){
         //get the question text
         return a;
     }
-    public String getAnswer(){
-        //get the answer as text
-        return b;
+    public String getAnswera(){
+        //get the answer a as text
+        return b1;
+    }
+    public String getAnswerb(){
+        //get the answer b as text
+        return b2;
+    }
+    public String getAnswerc(){
+        //get the answer c as text
+        return b3;
+    }
+    public String getAnswerd(){
+        //get the answer d as text
+        return b4;
     }
     public int getAnswerKey(){
         // get the abcd in a int form 1-4
@@ -44,8 +65,7 @@ public class TestingActivity extends Activity implements View.OnClickListener {
     Button c;
     Button d;
     ArrayList<Triple> list = new ArrayList<Triple>();
-
-
+    InputStream isans,isqs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +79,12 @@ public class TestingActivity extends Activity implements View.OnClickListener {
         c.setOnClickListener(this);
         d = (Button) findViewById(R.id.button_d);
         d.setOnClickListener(this);
+        try {
+            isqs  = getAssets().open("SpaceAppsQuestions.txt");
+            isans = getAssets().open("SpaceAppsAnswers.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
